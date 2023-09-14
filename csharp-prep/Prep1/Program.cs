2a69e9b821a843001b5286dd801be7f9912f3e45
Bernie Zhong <chu23008@byui.edu>
2023-09-15 07:49:12 +0800

    Prep1 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // writeline = 印完之後換行
+        // Console.WriteLine("Hello Prep1 World!");
+        
+        // = python input
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+        
+        // = python f string print out
+        Console.WriteLine($"\nYour name is {lastname}, {firstname} {lastname}.");
+
     }
 }
+// 程式碼需要打在上面的 program 裏面
+// Console.Write("What is your first name?");
+// string firstname = Console.Write();
