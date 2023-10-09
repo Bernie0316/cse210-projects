350c0e183e9d898d6d78914479005a3dd8038fe1
Bernie Zhong <chu23008@byui.edu>
2023-10-09 10:47:28 +0800

    finish the Assignment w04

---
+++ prove/Develop02/Class.cs
@@ -0,0 +1,93 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+public class GeneratePrompt
+{
+    public static string Prompt()
+    {
+        List<string> prompts = new List<string>
+        {
+            "Document your experiences and personal growth in school, work, or life in general.",
+            "List things and people you're grateful for and moments of happiness.",
+            "Write essays or poetry reflecting your thoughts, emotions, and creativity.",
+            "Explore your expectations, plans, and aspirations for the future.",
+            "Express your thoughts and reflections on social issues, news events, or political matters."
+        };
+        // 開始使用Random
+        Random random = new Random();
+        int index = random.Next(0, prompts.Count);
+        string sentence = prompts[index];
+        return sentence;
+    }
+}
+
+public class Journal
+{
+    public List<Entry> entries;
+    public Journal()
+    {
+        entries = new List<Entry>();
+    } 
+    public void AddEntry(DateTime date, string prompt, string content)
+    {
+        entries.Add(new Entry(date, prompt, content));
+    }
+    public void DisplayEntries()
+    {
+        foreach (Entry entry in entries)
+        {
+            entry.Display();
+        }
+    }
+    public void SaveToFile(string filename)
+    {
+        Console.WriteLine("Saving to file...");
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry entry in entries)
+            {
+                outputFile.WriteLine($"{entry._Date},{entry._Prompt},{entry._Content}");
+            }
+        }
+        Console.WriteLine("File saved successfully.");
+    }
+ 
+    public void ReadFromFile(string filename)
+    {
+        string[] lines = File.ReadAllLines(filename);
+ 
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(',');
+            if (parts.Length == 3)
+            {
+                DateTime dateOfFile = DateTime.Parse(parts[0]);
+                string promptOfFile = parts[1];
+                string contentOfFile = parts[2];
+                entries.Add(new Entry(dateOfFile, promptOfFile, contentOfFile));
+            }
+        }
+    }
+}
+
+
+public class Entry
+{
+    public DateTime _Date;
+    public string _Prompt;
+    public string _Content;
+ 
+    public Entry(DateTime date, string prompt, string content)
+    {
+        _Date = date;
+        _Prompt = prompt;
+        _Content = content;
+    }
+ 
+    public void Display()
+    {
+        Console.WriteLine($"Date: {_Date.ToShortDateString()} - Prompt: {_Prompt}");
+        Console.WriteLine($"{_Content}");
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,75 @@
 using System;
-
+using System.IO;
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        // Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        Console.WriteLine("Welcome to Bernie's Journal program");
+        List<string> choices = new List<string>
+        {
+            "1. Write",
+            "2. Display",
+            "3. load",
+            "4. Save",
+            "5. Quit"
+        };
+        int num;
+        do
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            foreach (string choice in choices)
+            {
+                Console.WriteLine(choice);
+            }
+            Console.Write("What would you like to do? ");
+            if (int.TryParse(Console.ReadLine(), out num))
+            {
+                if (num == 1) // Write with a random prompt
+                {
+                    string prompt = GeneratePrompt.Prompt();
+                    Console.WriteLine(prompt);
+                    string content = Console.ReadLine();
+                    journal.AddEntry(DateTime.Now, prompt, content);
+                    // break;  這功能為把這個while停止
+                }
+                else if (num == 2) // Display
+                {
+                    journal.DisplayEntries();
+                    // break;
+                }
+                else if (num == 3) // Load
+                {
+                    if (File.Exists("journal.txt"))
+                    {
+                        journal.ReadFromFile("jounal.txt");
+                        Console.WriteLine("File loaded successfully") ;
+                        journal.DisplayEntries();
+                    }
+                    else 
+                    {
+                        Console.WriteLine("File does not find");
+                    }
+                    // break;
+                }
+                else if (num == 4) // Save
+                {
+                    journal.SaveToFile("journal.txt");
+                    // break;
+                }
+                else if (num != 5)
+                {
+                    Console.WriteLine("Please enter number 1-5");
+                }
+            }
+            else  
+            {
+                Console.WriteLine("Please enter with NUMBER");
+            } 
+        }
+        while (num != 5);
+        Console.WriteLine("See you next time ~");
     }
 }
