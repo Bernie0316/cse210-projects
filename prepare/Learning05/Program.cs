60579972ccaa7f7de3c8e180572630cae9a3eac1
Bernie Zhong <chu23008@byui.edu>
2023-11-18 09:31:41 +0800

    Finished the L5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Console.WriteLine("Hello Learning05 World!");
+        List <Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Red", 10));
+        shapes.Add(new Rectangle("Blue", 10, 20));
+        shapes.Add(new Circle("Green", 10));
+
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"{color} - {area}");
+        }
     }
 }
